343c89c685d36b019262089728cf602702908a07
Raghuveer Phull <rphull@apple.com>
2016-06-02 22:44:57 -0700

    first round of css changes

--- app/scripts/controller/main.js
+++ app/scripts/controller/main.js
@@ -8,13 +8,10 @@
  * Controller of the websiteApp
  */
 angular.module('raghuveer')
-    .controller('MainController', [ '$scope', function ($scope) {
-        $scope.input =  ['Hello my friend!^2000\n'
-            + 'Welcome to the world of Raghuveer^500.^500.^500.^2000\n'
-            + 'I am Team Lead/Sr. Software Engineer at Apple.^2000\n'
-            + 'A tools developer for release automation by profession^500.^500.^500. and^1000\n'
+    .controller('MainController', [ '$scope', '$location', function ($scope, $location) {
+        $scope.input =  ['Hola!!^2000 I am Raghuveer^2500 currently working at Apple.^2000\n'
+            + 'A release tools developer by profession & ^2000\n'
             + 'web development enthusiast by heart^500.^500.^500.^2000\n'
-            + 'Food and Scotch are my weakness,^2000\n'
-            + 'while travelling to new places is my passion.^2000\n'
-            + 'Welcome to know me^500.^500.^500. Welcome to raghuveer.us '];
+            + 'Food and Scotch are my weakness,^2000 \nmusic is my motivation^2000 and ^2000\n'
+            + 'exploring new places is my passion.^2000\n'];
     }]);
