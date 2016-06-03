7524ecaf0e6379fbf9132185be86ebd2a235a5e8
Raghuveer Phull <rphull@apple.com>
2016-06-03 14:19:41 -0700

    changing introduction content

--- app/scripts/controller/main.js
+++ app/scripts/controller/main.js
@@ -9,9 +9,9 @@
  */
 angular.module('raghuveer')
     .controller('MainController', [ '$scope', '$location', function ($scope, $location) {
-        $scope.input =  ['Hola!!^2000 I am Raghuveer^2500 currently working at Apple.^2000\n'
-            + 'A release tools developer by profession & ^2000\n'
-            + 'web development enthusiast by heart^500.^500.^500.^2000\n'
-            + 'Food and Scotch are my weakness,^2000 \nmusic is my motivation^2000 and ^2000\n'
-            + 'exploring new places is my passion.^2000\n'];
+        $scope.input =  ['Hello my friend!^2000 My name is Raghuveer.^2500\nCurrently working at Apple,^1500 Yeah baby!^2000\n'
+            + 'I\'m a tools developer by profession &^2000\n'
+            + 'web development supporter by heart^500.^500.^500.^2000\n'
+            + 'Food is my love,^2000 scotch is my weakness,^2000\nlistening music makes me happy^2000 and ^2000\n'
+            + 'exploring new places gives me pleasure.^2000'];
     }]);
