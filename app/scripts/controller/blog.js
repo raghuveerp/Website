6c6332407f5f7f6029539b6c9394236b558764ce
Raghuveer Phull <raghuveersinghp@gmail.com>
2016-08-24 03:45:45 -0700

    blogs accordian sample

--- app/scripts/controller/blog.js
+++ app/scripts/controller/blog.js
@@ -9,12 +9,23 @@
  */
 angular.module('raghuveer')
     .controller('BlogController', function ($scope) {
-        // $scope.hoverIn = function(){
-        //     this.hoverEdit = true;
-        // };
-        //
-        // $scope.hoverOut = function(){
-        //     this.hoverEdit = false;
-        // };
+        var toggleSpeed = 300;
+        $('.toggle h4.active + .toggle-content').show();
 
+        $('.toggle h4').on("click", function () {
+            if ($(this).hasClass('active')) {
+                $(this).removeClass('active');
+                $(this).next('.toggle-content').stop(true, true).slideUp(toggleSpeed);
+            } else {
+                $(this).addClass('active');
+                $(this).next('.toggle-content').stop(true, true).slideDown(toggleSpeed);
+
+                //accordion
+                if ($(this).parents('.toggle-group').hasClass('accordion')) {
+                    $(this).parent().siblings().find('h4').removeClass('active');
+                    $(this).parent().siblings().find('.toggle-content').stop(true, true).slideUp(toggleSpeed);
+                }
+            }
+            return false;
+        });
     });
