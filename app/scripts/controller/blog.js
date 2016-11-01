f54a23b48148e40bef4e6e8598553be7f1fb3dba
Raghuveer Phull <raghuveersinghp@gmail.com>
2016-10-31 18:36:09 -0700

    Adding shayari and template page

--- app/scripts/controller/blog.js
+++ app/scripts/controller/blog.js
@@ -21,11 +21,18 @@ angular.module('raghuveer')
         $scope.blogsBaseURL = $location.$$absUrl;
 
         $scope.blogs = [
+            // {
+            //     title: 'CMPE 275: Setting up environment',
+            //     content: 'This blog was written for class CMPE 275 under Prof. Kevin Chang, on how to setup initial environment for this course.',
+            //     keyword: 'Setting-Up-Environment',
+            //     date: 'Feb 4, 2009'
+            // }
+
             {
-                title: 'CMPE 275: Setting up environment',
-                content: 'This blog was written for class CMPE 275 under Prof. Kevin Chang, on how to setup initial environment for this course.',
-                keyword: 'Setting-Up-Environment',
-                date: 'Feb 4, 2009'
+                title : 'Sher-Meri-Shayari ...',
+                content : 'This is my attempt to express myself in the form of shayari .. ',
+                keyword : 'Shayari',
+                date : 'October 31, 2016'
             }
         ];
 
